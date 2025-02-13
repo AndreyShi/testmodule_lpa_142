0df07ea831068c1b6e1bfde608c2e1b23ed47abe
a.shiliaev <andrey.shilyayev.89@mail.ru>
2025-02-13 11:37:16 +0300

    refactor 2

--- Src/usbd_cdc_if.c
+++ Src/usbd_cdc_if.c
@@ -22,6 +22,17 @@
 #include "usbd_cdc_if.h"
 
 /* USER CODE BEGIN INCLUDE */
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  ((byte) & 0x80 ? '1' : '0'), \
+  ((byte) & 0x40 ? '1' : '0'), \
+  ((byte) & 0x20 ? '1' : '0'), \
+  ((byte) & 0x10 ? '1' : '0'), \
+  ((byte) & 0x08 ? '1' : '0'), \
+  ((byte) & 0x04 ? '1' : '0'), \
+  ((byte) & 0x02 ? '1' : '0'), \
+  ((byte) & 0x01 ? '1' : '0') 
+
 #include "gpio_if.h"
 #include "tests.h"
 #include "adc_if.h"
@@ -455,6 +466,9 @@ void usb_parse(usb_packet* pk)
         enc_cnt_diag = 0;
     }else if(strcmp((const char*)UserRxBufferFS, "ручная диагностика выкл") == 0){
         btn_context = c_ChooseCh;
+    }else if(strcmp((const char*)UserRxBufferFS, "адрес платы") == 0){
+      //printf("id_read: %d\n",id_read());
+      printf("id_read: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(id_read()));
     }else
          { printf("неизвестная команда ☺\n");}
         
